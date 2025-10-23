5355596a7b04d35dbadda37fb65e24e4a570c6b8
AraReighard <arareighard@ku.edu>
2025-10-22 17:15:18 -0700

    uploaded everything from cycleserver

--- Lab08/FootballScore.c
+++ Lab08/FootballScore.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 
 int main() {
-    int TD = 6, FG = 3, SAFETY = 3, TDC = 8, TDFG = 7;
+    int TD = 6, FG = 3, SAFETY = 3, TDC = 8, TDFG = 7, score;
 
-    
+    while (1) {
+        printf("Enter the NFL score (Enter 1 to stop): ");
+        scanf("%d", &score);
+        printf("\n");
+
+        if (score == 1) {
+            printf("exiting...\n");
+            break;
+        }
+
+        else if (score > 0) {
+
+            printf("Possible combinations of scoring plays if a team's score is %d:\n", score);
+
+            for (int field_goals = 0; field_goals <= score / FG; field_goals++) {
+
+                for (int safeties = 0; safeties <= score / SAFETY; safeties++) {
+
+                    for (int touchdowns = 0; touchdowns <= score / TD; touchdowns++) {
+
+                        for (int touchdowns_with_field_goals = 0; touchdowns_with_field_goals <= score / TDFG; touchdowns_with_field_goals++) {
+
+                            for (int touchdowns_with_conversion = 0; touchdowns_with_conversion <= score / TDC; touchdowns_with_conversion++) {
+
+                                if (field_goals * FG + safeties * SAFETY + touchdowns * TD + touchdowns_with_field_goals * TDFG + touchdowns_with_conversion * TDC == score) 
+                                printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", touchdowns_with_conversion, touchdowns_with_field_goals, touchdowns, field_goals, safeties);
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        else {
+            printf("Please enter a positive integer\n");
+        }
+    }
 }
--- Lab08/Temperature.c
+++ Lab08/Temperature.c
@@ -4,14 +4,14 @@ int main() {
     char input_scale, target_scale;
     float input_temperature, c_temperature, target_temperature;
 
-    printf("Enter the temparture value: ");
-    getchar("%f", &input_temperature);
+    printf("Enter the temperature value: ");
+    scanf("%f", &input_temperature);
 
     printf("Input the original scale (C, F, or K): ");
-    getchar("%c", &input_scale);
+    scanf("%s", &input_scale);
 
     printf("Input the scale to convert (C, F, or K): ");
-    getchar("%c", &target_scale);
+    scanf("%s", &target_scale);
 
     if (input_scale == 'F') c_temperature = (input_temperature - 32.0) / 1.8;
     if (input_scale == 'C') c_temperature = input_temperature;
@@ -19,33 +19,33 @@ int main() {
 
     if (target_scale == 'F') target_temperature = c_temperature * 1.8 + 32;
     if (target_scale == 'C') target_temperature = c_temperature;
-    if (target_scale == 'F') target_temperature = c_temperature + 273.15;
+    if (target_scale == 'K') target_temperature = c_temperature + 273.15;
 
-    printf("Converted temperature: %f %c", target_temperature, target_scale);
+    printf("Converted temperature: %f %c\n", target_temperature, target_scale);
 
     if (c_temperature < 0) {
-        printf("Temperature Category: Freezing");
-        printf("Weather Advisory: Wear a coat");
+        printf("Temperature Category: Freezing\n");
+        printf("Weather Advisory: Wear a coat\n");
     }
 
     else if (c_temperature < 10) {
-        printf("Temperature Category: Cold");
-        printf("Weather Advisory: Wear a jacket");
+        printf("Temperature Category: Cold\n");
+        printf("Weather Advisory: Wear a jacket\n");
     }
 
     else if (c_temperature < 25) {
-        printf("Temperature Category: Comfortable");
-        printf("Weather Advisory: Take a nice stroll");
+        printf("Temperature Category: Comfortable\n");
+        printf("Weather Advisory: Take a nice stroll\n");
     }
 
     else if (c_temperature < 35) {
-        printf("Temperature Category: Hot");
-        printf("Weather Advisory: Drink lots of water!");
+        printf("Temperature Category: Hot\n");
+        printf("Weather Advisory: Drink lots of water!\n");
     }
 
     else {
-        printf("Temperature Category: Extreme Heat");
-        printf("Weather Advisory: Stay inside!");
+        printf("Temperature Category: Extreme Heat\n");
+        printf("Weather Advisory: Stay inside!\n");
     }
 
 }
