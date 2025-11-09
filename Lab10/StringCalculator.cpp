9610a8297ff15ae610f75a1b09e483697810edaf
AraReighard <arareighard@ku.edu>
2025-11-09 13:16:34 -0800

    math?

--- Lab10/StringCalculator.cpp
+++ Lab10/StringCalculator.cpp
@@ -43,23 +43,27 @@ string add(string num1, string num2) {
     // block to handle subtraction and addition and the signs
     if (num1[0] == '-' && num2[0] == '-') {
         subtractionSign = true;
+        num1.erase(0,1);
+        num2.erase(0,1);
     }
 
     if (num2[0] == '+') {
-        cout << 1 << endl;
+        // cout << 1 << endl;
         num2.erase(0,1);
     }
     else if (num2[0] == '-') {
-        cout << 2 << endl;
+        // cout << 2 << endl;
         subtraction = !subtraction;
+        num2.erase(0,1);
     }
 
     if (num1[0] == '+') {
-        cout << 3 << endl;
+        // cout << 3 << endl;
         num1.erase(0,1);
     }
     else if (num1[0] == '-') {
-        cout << 4 << endl;
+        // cout << 4 << endl;
+        num1.erase(0,1);
         string tempString = num2;
         num2 = num1;
         num1 = tempString;
@@ -76,14 +80,14 @@ string add(string num1, string num2) {
 
     
     // block to make them all the same size
-    while (num1Characteristic.size() < num2Characteristic.size()) {
-        num1Characteristic = "0" + num1Characteristic;
-        // cout << num1Characteristic << endl;
-    }
-    while (num1Characteristic.size() > num2Characteristic.size()) {
-        num2Characteristic = "0" + num2Characteristic;
-        // cout << num2Characteristic << endl;
-    }
+    // while (num1Characteristic.size() < num2Characteristic.size()) {
+    //     num1Characteristic = "0" + num1Characteristic;
+    //     // cout << num1Characteristic << endl;
+    // }
+    // while (num1Characteristic.size() > num2Characteristic.size()) {
+    //     num2Characteristic = "0" + num2Characteristic;
+    //     // cout << num2Characteristic << endl;
+    // }
     while (num1Mantissa.size() < num2Mantissa.size()) {
         num1Mantissa = "0" + num1Mantissa;
         // cout << num1Characteristic << endl;
@@ -93,19 +97,50 @@ string add(string num1, string num2) {
         // cout << num2Characteristic << endl;
     }
 
-    cout << "num1Char: " << num1Characteristic << endl;
-    cout << "num2Char: " << num2Characteristic << endl;
-    cout << "num1Man: " << num1Mantissa << endl;
-    cout << "num2Man: " << num2Mantissa << endl;
+    // cout << "num1Char: " << num1Characteristic << endl;
+    // cout << "num2Char: " << num2Characteristic << endl;
+    // cout << "num1Man: " << num1Mantissa << endl;
+    // cout << "num2Man: " << num2Mantissa << endl;
     // cout << num1Characteristic + '.' + num1Mantissa << endl;
     // cout << num2Characteristic + '.' + num2Mantissa << endl;
 
-    
+    int mantissaLength = num1Mantissa.size();
+
+    string mantissaSum = "help";
+    string characteristicSum = "help2";
+
+    if (!subtraction) {
+        mantissaSum = to_string(stoi(num1Mantissa) + stoi(num2Mantissa));
+        characteristicSum = to_string(stoi(num1Characteristic) + stoi(num2Characteristic));
+
+        if (mantissaSum.size() > mantissaLength) {
+            characteristicSum = to_string(stoi(characteristicSum) + 1);
+            mantissaSum.erase(0,1);
+        }
+    }
+
+    else {
+        mantissaSum = to_string(stoi(num1Mantissa) - stoi(num2Mantissa));
+        characteristicSum = to_string(stoi(num1Characteristic) - stoi(num2Characteristic));
+
+        bool mantissaNegative = (mantissaSum[0] == '-') ? true : false;
+        if (mantissaNegative) mantissaSum.erase(0,1);
+
+        if (mantissaNegative) {
+            if (mantissaSum.size() > mantissaLength) {
+                characteristicSum = to_string(stoi(characteristicSum) - 2);
+                mantissaSum.erase(0,1);
+            }
+            else {
+                characteristicSum = to_string(stoi(characteristicSum) - 1);
+                mantissaSum = to_string(1 - stoi(mantissaSum));
+            }
+        }
+    }
 
 
 
-
-    return "ignore this line";
+    return characteristicSum + '.' + mantissaSum;
 }
 
 string recAdd(string num1, string num2) {
